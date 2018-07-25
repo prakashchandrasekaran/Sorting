72d136a493644d95e2d476374296c7affaae3685
Prakash Chandrasekaran <prakashc@outlook.com>
2018-07-25 00:19:00 -0700

    Add SelectionSort algo

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -26,11 +26,45 @@ void InsertionSort(int arr[], int n)
     }
 }
 
+void SelectionSort(int arr[], int n)
+{
+    if (arr == nullptr)
+    {
+        return;
+    }
+
+    if (n <= 1)
+    {
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int min = arr[i];
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (min > arr[j])
+            {
+                min = arr[j];
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i)
+        {
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
 int main()
 {
     int arr[] = { 2,5,3,7,9,11,10,1,0 };
     int n = sizeof(arr) / sizeof(arr[0]);
-    InsertionSort(arr, n);
+    SelectionSort(arr, n);
 
     for (int i = 0; i < n; i++)
     {
